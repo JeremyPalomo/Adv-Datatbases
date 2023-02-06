047c6cc3c7307e49951dc3de15df4de83b16f90d
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-02-05 21:16:33 -0600

    fef

--- geonanesv2.sql
+++ geonanesv2.sql
@@ -1,8 +1,8 @@
-create table raw.admin1
+create table admin1
 (
   code text,
   name text,
   ascii_name text,
   geonameid bigint,
 );
-\copy raw.admin1 from '/var/local/cs4443/geonames/admin1CodesASCII.txt' with csv delimiter E'\t'
+\copy admin1 from '/var/local/cs4443/geonames/admin1CodesASCII.txt' with csv delimiter E'\t'
