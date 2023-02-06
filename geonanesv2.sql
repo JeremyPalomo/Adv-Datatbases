4e779e83c34e86416dfb4a0eb0b255d11049e761
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-02-05 21:48:21 -0600

    fef

--- geonanesv2.sql
+++ geonanesv2.sql
@@ -1,7 +1,45 @@
-CREATE TABLE admin1 (
+CREATE TABLE admin1_temp (
   code text,
   name text,
   ascii_name text,
   geonameid bigint
 );
-\copy admin1 from '/var/local/cs4443/geonames/admin1CodesASCII.txt' with csv delimiter E'\t'
+\copy admin1_temp from '/var/local/cs4443/geonames/admin1CodesASCII.txt' with csv delimiter E'\t'
+
+CREATE TABLE admin2_temp (
+  code text,
+  name text,
+  ascii_name text,
+  geonameid bigint
+);
+\copy admin2_temp from '/var/local/cs4443/geonames/admin2Codes.txt' with csv delimiter E'\t'
+
+create table feature_temp (
+    code text,
+    description text,
+    comment text
+);
+\copy feature_temp from '/var/local/cs4443/geonames/featureCodes_en.txt' with csv delimiter E'\t'
+
+create table country_temp(
+    iso text,
+    iso3 text,
+    isocode integer,
+    fips text,
+    name text,
+    capital text,
+    area double precision,
+    population bigint,
+    continent text,
+    tld text,
+    currency_code text,
+    currency_name,
+    phone text,
+    postal_code_format text,
+    postal_code_regex text,
+    languages text,
+    geonamesid bigint,
+    neighbors text,
+    fips_equiv text
+);
+\copy country_temp from '/var/local/cs4443/geonames/countryInfoData.txt' with csv delimiter E'\t'
