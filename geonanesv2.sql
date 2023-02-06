024638e6e4530151855525554400fe8c82f726b6
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-02-05 21:14:50 -0600

    fef

--- geonanesv2.sql
+++ geonanesv2.sql
@@ -1,33 +1,8 @@
-
-
-CREATE TABLE geonames (
-    geoname_id bigint,
-    name text,
-    ascii_name text,
-    alternate_names text,
-    latitude double precision,
-    longitude double precision,
-    f_class text,
-    f_code text,
-    cc1 text,
-    cc2 text,
-    admin1 text,
-    admin2 text,
-    admin3 text,
-    admin4 text,
-    population bigint,
-    elevation bigint,
-    dem bigint,
-    time text,
-    mod_date date
+create table raw.admin1
+(
+  code text,
+  name text,
+  ascii_name text,
+  geonameid bigint,
 );
-
-load csv
-  from /var/local/cs4443/geonames/allCountries.txt
-  into pgsql://appdev@/appdev
-  target table geonames
-
-  with fields terminated by '\t',
-       fields optionally enclosed by '§',
-       fields escaped by '%',
-       truncate;
+\copy raw.admin1 from 'admin1CodesASCII.txt' with csv delimiter E'\t'
