6e3a4286eb5b9d792b6157db45d499e38b516508
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-02-13 08:46:29 -0600

    f

--- geonanesv2.sql
+++ geonanesv2.sql
@@ -136,4 +136,4 @@ CREATE TABLE timezones(
 INSERT INTO timezones(country_code, timezone_name, offsets)
    SELECT  DISTINCT country_codes, country_timezone, country_globaloffset
    FROM    rawtime
-   WHERE   timezone_name IS NOT NULL;
+   WHERE   country_timezone IS NOT NULL;
