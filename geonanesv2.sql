baf737275027e8265cccf362077303c7b1c14f71
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-02-13 07:53:02 -0600

    j

--- geonanesv2.sql
+++ geonanesv2.sql
@@ -1,45 +1,46 @@
-CREATE TABLE admin1_temp (
-  code text,
-  name text,
-  ascii_name text,
-  geonameid bigint
+CREATE TABLE rawgeonames(
+    geonameid       BIGINT,
+    name            text,
+    asciiname       text,
+    alternames      text,
+    latitude        double precision,
+    longitude       double precision,
+    class           text,
+    code            text,
+    cc2             text,
+    admin1          text,
+    admin2          text,
+    admin3          text,
+    admin4          text,
+    population     bigint,
+    elevation       bigint,
+    dem             bigint,
+    timezone        text,
+    modification    date
 );
-\copy admin1_temp from '/var/local/cs4443/geonames/admin1CodesASCII.txt' with csv delimiter E'\t'
 
-CREATE TABLE admin2_temp (
-  code text,
-  name text,
-  ascii_name text,
-  geonameid bigint
-);
-\copy admin2_temp from '/var/local/cs4443/geonames/admin2Codes.txt' with csv delimiter E'\t' || csv delimiter E'517838'
 
-create table feature_temp (
-    code text,
-    description text,
-    comment text
+CREATE TABLE rawcountry(
+    iso             text,
+    iso3            text,
+    isocode         int,
+    fips            text,
+    name            text,
+    capital         text,
+    area            double precision,
+    population      bigint,
+    continent       text,
+    tld             text,
+    currency_code   text,
+    currency_name   text,
+    phone           text,
+    postalcodef     text,
+    postalcoder     text,
+    languages       text,
+    geonameid       bigint,
+    neighbors       text,
+    fips_equiv      text
 );
-\copy feature_temp from '/var/local/cs4443/geonames/featureCodes_en.txt' with csv delimiter E'\t'
 
-create table country_temp(
-    iso text,
-    iso3 text,
-    isocode integer,
-    fips text,
-    name text,
-    capital text,
-    area double precision,
-    population bigint,
-    continent text,
-    tld text,
-    currency_code text,
-    currency_name text,
-    phone text,
-    postal_code_format text,
-    postal_code_regex text,
-    languages text,
-    geonamesid bigint,
-    neighbors text,
-    fips_equiv text
-);
-\copy country_temp from '/var/local/cs4443/geonames/countryInfo2.txt' with csv delimiter E'\t'
+\copy rawcountry from '/var/local/cs4443/geonames/countryInfo2.txt' with csv delimiter E'\t'
+
