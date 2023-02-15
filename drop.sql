4569c7684437f1782eb0e015c4d083295cdf11f1
Jeremy Palomo <jeremypalomo02@gmail.com>
2023-02-14 23:46:00 -0600

    f

--- drop.sql
+++ drop.sql
@@ -1,1 +1,1 @@
-drop table continents, timezones, features,coordinates,boundry,languages,currency,countries,alternatenames,geonames,hierarchy CASCADE; 
+drop table continents, timezones, features,coordinates,boundry,languages,currency,countries,geonames,hierarchy CASCADE; 
